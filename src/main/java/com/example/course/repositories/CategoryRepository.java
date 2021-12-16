5042922ed1ff6172072b8b50ada8c6bcbc31772b
lewisc99 <lewiscontato99@gmail.com>
2021-12-16 17:11:31 -0300

    Product

---
+++ src/main/java/com/example/course/repositories/CategoryRepository.java
@@ -0,0 +1,20 @@
+ package com.example.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.course.entities.Category;
+
+
+
+@Repository //always when need to use inject dependency
+//you need to put this prefixix @ (name of the Dependency)
+//and in the class that will use dependency inject.
+//put @Autowired, in this class don't necessary need to add
+//@repository because already implements JapRepository.
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+	
+	
+	
+
+}
